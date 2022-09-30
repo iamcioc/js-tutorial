acc6f3e0d0e73dd0b067dce0cacd2c95c43831d9
czar <caesarchuks12@yahoo.com>
2022-09-30 18:06:59 -0400

    conditional-operators

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // food = Number (prompt('What do you want to'))
-// tipPercentage = Number(prompt('tip %')/100) 
-// tipAmount = food * tipPercentage 
-// total = food + tipAmount 
+// tipPercentage = Number(prompt('tip %')/100)
+// tipAmount = food * tipPercentage
+// total = food + tipAmount
 
 // console.log('tip amount', tipAmount)
 // console.log('total', total)
@@ -9,6 +9,16 @@
 
 // Data types
 
-console.log(Math.floor(Math.random() * 3));
-console.log(Math.ceil(4.2873))
-console.log(Math.floor(2.8838383))
+// console.log(Math.floor(Math.random() * 3));
+// console.log(Math.ceil(4.2873))
+// console.log(Math.floor(2.8838383))
+
+// Baby weather app
+let weather = prompt('how is the weather')
+
+if (weather == 'rainy') {
+    console.log('grab your umbrella')
+} else {
+        console.log('wear your sunglasses')
+    }
+

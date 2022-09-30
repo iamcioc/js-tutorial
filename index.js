260220d20f608f49c3e24461d84ec051cf858e4a
czar <caesarchuks12@yahoo.com>
2022-09-30 16:02:22 -0400

    simple-math-mehod

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-food = Number (prompt('What do you want to'))
-tipPercentage = Number(prompt('tip %')/100) 
-tipAmount = food * tipPercentage 
-total = food + tipAmount 
+// food = Number (prompt('What do you want to'))
+// tipPercentage = Number(prompt('tip %')/100) 
+// tipAmount = food * tipPercentage 
+// total = food + tipAmount 
 
-console.log('tip amount', tipAmount)
-console.log('total', total)
+// console.log('tip amount', tipAmount)
+// console.log('total', total)
 
 
+// Data types
+
+console.log(Math.floor(Math.random() * 3));
+console.log(Math.ceil(4.2873))
+console.log(Math.floor(2.8838383))

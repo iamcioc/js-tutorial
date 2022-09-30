ca32e45f27b786dbe25211fc8040a52952330249
czar <caesarchuks12@yahoo.com>
2022-09-30 14:35:02 -0400

    simple-calculation

--- index.js
+++ index.js
@@ -1,22 +1,9 @@
-const h1 = document.querySelector("h1")
+food = Number (prompt('What do you want to'))
+tipPercentage = Number(prompt('tip %')/100) 
+tipAmount = food * tipPercentage 
+total = food + tipAmount 
 
-fetch("https://jsonplaceholder.typicode.com/posts")
-    .then((res) => {
-        return res.json()
-    })
-    .then((data) => {
-        data.map((item) => {
-            const h2 = document.createElement("h2");
-            const h3 = document.createElement("h3");
-            const h4 = document.createElement("h4");
-            h2.innerHTML = `Title: ${item.title}`;
-            h3.innerHTML = `ID: ${item.id}`;
-            h4.innerHTML = `Body: ${item.body}`;
+console.log('tip amount', tipAmount)
+console.log('total', total)
 
-            h1.appendChild(h2);
-            h1.appendChild(h3);
-            h1.appendChild(h4);
 
-        });
-    })
-    .catch((err) => console.log(err));
